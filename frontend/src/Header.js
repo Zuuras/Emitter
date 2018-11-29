c0f9891a14f14a4a2a8ccac5c09519f8aaba9296
TheJoiz <oleganishe123@gmail.com>
2018-11-29 21:57:32 +0200

    header v1.0

---
+++ frontend/src/Header.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import './Header.scss'
+
+class Header extends Component{
+    render(){
+        
+        return(<div className="main-header">
+                <div className="logo-head" >
+                    <a href="http://localhost:3000/" >Emitter_98</a>
+                    <div className="btn favorites" onClick=>
+                    </div>
+                    <div className="btn notifications">
+                    </div>
+                    <div className="btn messages">
+                    </div>
+                </div>
+                <div className="welcome">
+                    Welcome to the 1998
+                </div>
+            <audio id="sound-link" preload="auto">
+                <source src="audio/click.mp3" type="audio/mpeg"/>
+
+            </audio>
+
+            </div>
+
+        );
+
+
+    }
+
+}
+export default Header;
