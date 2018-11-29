4d997eb61bcef9cfe115297440c8982777c444a6
Arsenii Derkach <arsenii.derkach@gmail.com>
2018-11-29 19:34:12 +0200

    Back fix

--- backend/src/main/java/com/emitter/backend/entity/User.java
+++ backend/src/main/java/com/emitter/backend/entity/User.java
@@ -1,24 +1,24 @@
-package com.emitter.backend.entity;
-
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import lombok.Data;
-
-@Entity
-@Table(name="user")
-@Data
-public class User {
-  private String  userLogin;
-  private String  userPassword;
-  private String  userRole;
-  private String  userName;
-  private String  userSurname;
-  private String  nickname;
-  private String  userAdress;
-  private String  userPhone;
-  private String  userMail;
-  private String  userPhoto;
-  private String  userGender;
-  private Long userId;
-  }
+//package com.emitter.backend.entity;
+//
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//import lombok.Data;
+//
+//@Entity
+//@Table(name="user")
+//@Data
+//public class User {
+//  private String  userLogin;
+//  private String  userPassword;
+//  private String  userRole;
+//  private String  userName;
+//  private String  userSurname;
+//  private String  nickname;
+//  private String  userAddress;
+//  private String  userPhone;
+//  private String  userMail;
+//  private String  userPhoto;
+//  private String  userGender;
+//  private Long userId;
+//  }
