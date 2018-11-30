bc856781948fb22a94f8f6474d585604db017496
Zuuras <44036358+Zuuras@users.noreply.github.com>
2018-11-30 22:10:17 +0200

    Joiz/header (#1)
    * header v1.0
    
    * header v1.1
    
    * header v1.2
    
    * Header v2.0

---
+++ frontend/src/Header.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+import './Header.scss'
+
+class Header extends Component{
+    render(){
+
+        return(<div className="main-header">
+                <div className="logo-head" >
+                  <div className="wrap">
+                    <a href="http://localhost:3000/" >Emitter_98
+                    </a>
+
+                    <div className="btn favorites">
+                    </div>
+
+                    <div className="btn notifications">
+                    </div>
+
+                    <div className="btn messages">
+                    </div>
+
+                    <div className="search">
+                      <div className="search-icon">
+                      </div>
+
+                      <input  className="search-input" placeholder="Back to the future...">
+                      </input>
+                    </div>
+                    </div>
+                  <p className="phrase">&#9993; Luke i am your father ! &#9993;</p>
+                    <div className="btn profile">
+                        <p>OldMan228</p>
+                        <div className="profile-img"/>
+                    </div>
+                </div>
+                <div className="welcome">
+                    Welcome to the 1998
+                </div>
+            <audio id="sound-link" preload="auto">
+                <source src="audio/click.mp3" type="audio/mpeg"/>
+
+            </audio>
+
+            </div>
+
+        );
+
+
+    }
+
+}
+export default Header;
---
+++ frontend/src/funck.js
@@ -0,0 +1,9 @@
+
+function click()
+{
+
+    var soundLink = $("#sound-link")[0];
+
+    soundLink.play();
+}
+
