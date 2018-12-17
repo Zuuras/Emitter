3838120a5694a73f55ea6cfb4cabe3062f518268
Zuuras <mazarchuk13@gmail.com>
2018-12-17 22:06:45 +0200

    Message + chat started

--- frontend/src/components/Messages/Messages.js
+++ frontend/src/components/Messages/Messages.js
@@ -1,19 +1,25 @@
 import React, {Component} from 'react'
 import './messages.scss'
-import { connect } from "react-redux";
-
+import {connect} from "react-redux";
 
 
 class Messages extends Component {
     render() {
         return (
-            <div className="message-container" id="messages">
-                <div className="message">
-                    <div className="message_avatar"/>
-                    <div className="message_text-post">98 pirates on the worm from the desert</div>
-                    <div className="message_post-date">XX.XX.19XX</div>
+            <div className="messages-container">
+                <div className="message-container" id="messages">
+                    <div className="message message-btn">
+                        <div className="message_avatar"/>
+                        <div className="message_text-post">98 pirates on the worm from the desert</div>
+                        <div className="message_post-date">XX.XX.19XX</div>
+                    </div>
+                    <p className="main-text">There is no oldfags yet...</p>
                 </div>
-                <p className="main-text">There is no oldfags yet...</p>
+                <div className="chat-container" id="chat">
+                    <div className="chat">There is no spoon</div>
+                    <input  className="chat_input" placeholder="Back to the future..."/>
+                    <div className="chat_send-button">Send</div>
+                </div>
             </div>
         )
     }
