e725634f734599b7465ff370ed57ef71bb5a604e
Zuuras <mazarchuk13@gmail.com>
2018-12-19 21:19:29 +0200

    CREATE: Chat js and scss; now onClick show chat div UPDATE: Messages become nicer ADD: pirate image png (Strongly need to rename)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
         })
     }
     render() {
-        // console.log('App', this.props);
         return (
 
             <div className="App">
--- frontend/src/components/Messages/Messages.js
+++ frontend/src/components/Messages/Messages.js
@@ -1,25 +1,31 @@
 import React, {Component} from 'react'
 import './messages.scss'
 import {connect} from "react-redux";
+import Chat from './Chat/Chat'
 
 
 class Messages extends Component {
+    state = {
+        showed: false
+    }
+
+    ShowedToggle = () => {
+        this.setState({
+            showed: !this.state.showed
+        })
+    }
     render() {
         return (
             <div className="messages-container">
                 <div className="message-container" id="messages">
-                    <div className="message message-btn">
+                    <div className="message message-btn" onClick={this.ShowedToggle}>
                         <div className="message_avatar"/>
                         <div className="message_text-post">98 pirates on the worm from the desert</div>
                         <div className="message_post-date">XX.XX.19XX</div>
                     </div>
                     <p className="main-text">There is no oldfags yet...</p>
                 </div>
-                <div className="chat-container" id="chat">
-                    <div className="chat">There is no spoon</div>
-                    <input  className="chat_input" placeholder="Back to the future..."/>
-                    <div className="chat_send-button">Send</div>
-                </div>
+                {this.state.showed && <Chat/>}
             </div>
         )
     }
