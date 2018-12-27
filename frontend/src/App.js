13616f80fbc97f851705119beca31d635f6cbba0
Zuuras <mazarchuk13@gmail.com>
2018-12-27 19:15:08 +0200

    Feed component starting...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.scss';
 import Header from "./components/Header/Header";
 import Messages from "./components/Messages/Messages";
+import Feed from "./components/Feed/Feed";
 
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
             <div className="App">
                 <Header toggle={this.showedToggle} />
                 {this.state.showed && <Messages />}
+                <Feed/>
             </div>
         );
     }
