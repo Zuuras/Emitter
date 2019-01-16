c230b42889566a59e77ff1f236ddb60d9616b542
Zuuras <44036358+Zuuras@users.noreply.github.com>
2019-01-16 20:05:53 +0200

    Zuuras/message component (#4)
    * User entity v 0.1
    
    * User entity v 0.2
    
    * FIX: User entity
    add Lombok @Data
    
    * Back fix
    
    * try add Entity
    
    * //package com.emitter.backend.entity;
    
    * add new entity
    add temporary h2 database
    
    * User entity add v 0.1
    
    * User entity add v 0.1
    
    * UserController added
    
    * fix package-lock.json
    
    * start Messages component creation
    
    * Style fix
    
    * redux //still not working
    
    * messages showed
    
    * Message + chat started
    
    * CREATE: Chat js and scss; now onClick show chat div
    UPDATE: Messages become nicer
    ADD: pirate image png (Strongly need to rename)
    
    * default-case in reducer.js
    
    * default-case in reducer.js v.0.3
    
    * Feed component starting...
    
    * Feed element update v 0.2
    
    * create fixed form of Feed
    update design
    
    * Feed v0.3
    
    create empty Notif and Profile
    
    * for the travis...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Header from './Header';
 import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from "react-redux";
+import {createStore} from 'redux';
+import reducer from './reducers/reducer'
 
-ReactDOM.render(<Header />, document.getElementById('head'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+, document.getElementById('head'));
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
